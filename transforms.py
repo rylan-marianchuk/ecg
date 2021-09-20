77531ab6b4f602ab7c7d1d56b486bce37009c087
rylan-marianchuk <rylanmarianchuk@gmail.com>
2021-09-20 14:36:14 -0600

    first draft of FourierWindow

--- transforms.py
+++ transforms.py
@@ -1,7 +1,8 @@
 """
 transforms.py
 
-
+classes implementing common digital signal transformations:
+Fourier, WindowedFourier, Wavelet
 
 @author Rylan Marianchuk
 September 2021
@@ -45,18 +46,27 @@ class Fourier(object):
 
 
 class FourierWindowed(object):
-    def __init__(self, radius, step, T=10, ts=500):
-        self.radius = radius
+    def __init__(self, window, step, T=10, ts=500):
+        self.window = window
         self.step = step
         self.ts = ts
         self.T = T
-        # X values that couple with the return structure of __call(sample)__
-        # For display/figure generation
-        self.domain = periodogram(torch.zeros(self.ts * self.T), self.ts)[0]  # Frequency domain
+
+        # Hacky but correct for now until I find a closed form
+        l = []
+        for i in range(0, 5000, step):
+            if i + window >= 5000: break
+            l.append(i)
+        # 2D domain (image)
+        self.domain = periodogram(torch.zeros(self.ts * self.T), self.ts)[0].shape[0], len(l)
         return
 
     def __call__(self, signal):
-        return torch.fft.rfftfreq(signal)
+        image = torch.zeros(self.domain)
+        for i in range(0, self.T * self.ts, self.step):
+            fourier = periodogram(signal[i:i+self.window], self.ts)[1]
+            image[:, i] = fourier
+        return image
 
 
 class Wavelet(object):
